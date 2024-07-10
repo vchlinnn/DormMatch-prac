fe93dc6022c7b57f3ad32e2f1b477a98f7ca572c
vchlinnn <chuclinh190305@gmail.com>
2024-07-10 15:29:37 -0500

    Debug Fetching Users

--- App.js
+++ App.js
@@ -1,35 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, Alert } from 'react-native'
-import Constants from 'expo-constants'
-import TopBar from './components/TopBar'
-import axios from 'axios'
-import SwipeableImage from './components/SwipeableImage'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Alert, Text } from 'react-native';
+import Constants from 'expo-constants';
+import TopBar from './components/TopBar';
+import axios from 'axios';
+import SwipeableImage from './components/SwipeableImage';
 
 export default function App() {
 
-  const [users, setUsers] = useState([])
-  const [currentIndex, setCurrentIndex] = useState(0)
-  
+  const [users, setUsers] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   async function fetchUsers() {
     try {
-      const {data} = await axios.get('https://randomuser.me/api/')
-      setUsers(data.results)
+      const { data } = await axios.get('https://randomuser.me/api/');
+      setUsers(data.results);
       // console.log(data.results)
     } catch (error) {
-      console.log(error)
-      Alert.alert('Error getting users', '', [{text: 'Retry', onPress: () => fetchUsers()}])
+      console.log(error);
+      Alert.alert('Error getting users', '', [{ text: 'Retry', onPress: () => fetchUsers() }]);
     }
   }
 
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
 
   return (
     <View style={styles.container}>
       <TopBar />
       <View style={styles.swipes}>
-        {users.length > 1 && <SwipeableImage user={users[currentIndex]} />}
+        {users.length > 0 ? (
+          <SwipeableImage user={users[currentIndex]} />
+        ) : (
+          <Text>Loading...</Text>
+        )}
       </View>
     </View>
   );
--- components/SwipeableImage.js
+++ components/SwipeableImage.js
@@ -1,53 +1,59 @@
-import React from 'react'
-import { View, Image, StyleSheet, Text } from 'react-native'
-import { FontAwesome } from '@expo/vector-icons'
+import React from 'react';
+import { View, Image, StyleSheet, Text } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function SwipeableImage({ user }) {
     return (
-        <View>
-            <Image source={{ uri: user.picture.large }} style ={styles.photo} />
-            <View style={style.textContainer}>
+        <View style={styles.container}>
+            <Image source={{ uri: user.picture.large }} style={styles.photo} />
+            <View style={styles.textContainer}>
                 <View style={styles.textRow}>
                     <Text style={[styles.textPrimary, styles.textShadow]}>{user.name.first}</Text>
                     <Text style={[styles.textSecondary, styles.textShadow]}>{user.dob.age}</Text>
                 </View>
                 <View style={styles.textRow}>
-                    <FontAwesome name = "map-marker" size={20} color="white"/>
+                    <FontAwesome name="map-marker" size={20} color="white" />
                     <Text style={[styles.textSecondary, styles.textShadow]}>{user.location.city}</Text>
                 </View>
             </View>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     photo: {
-        height: '100%',
+        width: '100%',
+        height: 300,
         resizeMode: 'cover',
         borderRadius: 20,
     },
     textContainer: {
         position: 'absolute',
         bottom: 20,
-        left: 20
+        left: 20,
     },
     textRow: {
         flexDirection: 'row',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     textPrimary: {
         color: 'white',
         fontSize: 35,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
     },
     textSecondary: {
         color: 'white',
         fontSize: 25,
-        marginLeft: 10
+        marginLeft: 10,
     },
     textShadow: {
         textShadowColor: 'rgba(0, 0, 0, 0.80)',
         textShadowOffset: { width: -1, height: 1 },
         textShadowRadius: 10,
     },
-})
+});
