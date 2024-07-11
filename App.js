5ffb8d6f15144f9a33272dbead0613d867624372
vchlinnn <chuclinh190305@gmail.com>
2024-07-11 18:58:37 -0500

    Add swiping profiles feature

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Constants from 'expo-constants';
 import TopBar from './components/TopBar';
 import axios from 'axios';
 import SwipeableImage from './components/SwipeableImage';
+import Swipes from './components/Swipes';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
 
@@ -14,30 +16,29 @@ export default function App() {
     try {
       const { data } = await axios.get('https://randomuser.me/api/?results=5000');
       setUsers(data.results);
-      // console.log(data.results)
+      console.log("get data"); 
     } catch (error) {
       console.log(error);
       Alert.alert('Error getting users', '', [{ text: 'Retry', onPress: () => fetchUsers() }]);
     }
-  }
+  }  
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <TopBar />
       <View style={styles.swipes}>
-        {users.length > 1 ? (
-          <SwipeableImage user={users[currentIndex]} />
+        {users.length > 0 ? (
+          <Swipes currentIndex={currentIndex} users={users}></Swipes>
         ) : (
           <Text>Loading...</Text>
         )}
       </View>
-    </View>
-  );
-}
+    </GestureHandlerRootView>
+  )}
 
 const styles = StyleSheet.create({
   container: {
