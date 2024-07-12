f30a330c52117f55d372788a36ad68e97516dba1
vchlinnn <chuclinh190305@gmail.com>
2024-07-11 22:27:38 -0500

    Update Homescreen with Swipeable Feature

--- App.js
+++ App.js
@@ -1,44 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Alert, Text } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, Alert } from 'react-native';
 import Constants from 'expo-constants';
 import TopBar from './components/TopBar';
 import axios from 'axios';
-import SwipeableImage from './components/SwipeableImage';
+import BottomBar from './components/BottomBar';
 import Swipes from './components/Swipes';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
-
   const [users, setUsers] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const swipesRef = useRef(null);
 
   async function fetchUsers() {
     try {
-      const { data } = await axios.get('https://randomuser.me/api/?results=5000');
+      const { data } = await axios.get('https://randomuser.me/api/?results=50');
       setUsers(data.results);
-      console.log("get data"); 
     } catch (error) {
       console.log(error);
       Alert.alert('Error getting users', '', [{ text: 'Retry', onPress: () => fetchUsers() }]);
     }
-  }  
+  }
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
+  function handleLike() {
+    console.log('like');
+    nextUser();
+  }
+
+  function handlePass() {
+    console.log('pass');
+    nextUser();
+  }
+
+  function nextUser() {
+    const nextIndex = users.length - 2 === currentIndex ? 0 : currentIndex + 1;
+    setCurrentIndex(nextIndex);
+  }
+
+  function handleLikePress() {
+    swipesRef.current.openLeft();
+  }
+  function handlePassPress() {
+    swipesRef.current.openRight();
+  }
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <TopBar />
       <View style={styles.swipes}>
-        {users.length > 0 ? (
-          <Swipes currentIndex={currentIndex} users={users}></Swipes>
-        ) : (
-          <Text>Loading...</Text>
-        )}
+        {users.length > 1 &&
+          users.map(
+            (u, i) =>
+              currentIndex === i && (
+                <Swipes
+                  key={i}
+                  ref={swipesRef}
+                  currentIndex={currentIndex}
+                  users={users}
+                  handleLike={handleLike}
+                  handlePass={handlePass}
+                />
+              )
+          )}
       </View>
+      <BottomBar handleLikePress={handleLikePress} handlePassPress={handlePassPress} />
     </GestureHandlerRootView>
-  )}
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -50,5 +82,12 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingTop: 8,
     shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
   },
 });
